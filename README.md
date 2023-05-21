7d482bd10467d0e63927990956e8c783d4bd4a05
awakelife93 <awakelife93@gmail.com>
2023-05-21 17:28:12 +0900

    fix - update README.md

--- README.md
+++ README.md
@@ -26,19 +26,15 @@ Example Output
 
 ```javascript
 // default value
-{
-  origin: false,
-  exportPath: undefined,
-  extensionsJsonPath: undefined,
-}
-extensionDataGenerator({
+const options = {
   // Default is false, and this option exports the extensions metadata as it is.
   origin: false,
   // Default is outputs folder in Project root
   exportPath: undefined,
   // The default value of this option is vscode path on mac os, please put your correct path.
   extensionsJsonPath: undefined,
-});
+};
+extensionDataGenerator(options);
 ```
 
 ## Author
--- sample.js
+++ sample.js
@@ -1,13 +1,14 @@
 try {
   const extensionDataGenerator = require("./extensionDataGenerator");
-  extensionDataGenerator({
+  const options = {
     // Default is false, and this option exports the extensions metadata as it is.
     origin: false,
     // Default is outputs folder in Project root
     exportPath: undefined,
     // The default value of this option is vscode path on mac os, please put your correct path.
     extensionsJsonPath: undefined,
-  });
+  };
+  extensionDataGenerator(options);
 } catch (error) {
   console.error("Generate Extensions Data Json Failed", error);
 }
