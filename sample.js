69e2cf7b91d49684f0f784190a589f8039c8c5d1
awakelife93 <awakelife93@gmail.com>
2023-05-21 17:17:45 +0900

    fix - update sample, README.md origin option

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 try {
   const extensionDataGenerator = require("./extensionDataGenerator");
   extensionDataGenerator({
-    origin: true, // Default is false, and this option exports the extensions metadata as it is.
+    origin: false, // Default is false, and this option exports the extensions metadata as it is.
     exportPath: undefined, // Default is outputs folder in Project root
     extensionsJsonPath: undefined, // The default value of this option is vscode path on mac os, please put your correct path.
   });
